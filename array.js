fe2acf83fcf7be4fd1928bac00b194b0a8c2d2cd
amitkrec@gmail.com <amitkrec@gmail.com>
2024-02-14 23:59:28 +0530

    Concept of Array

---
+++ array.js
@@ -0,0 +1,6 @@
+
+const userArray = [2,54,63,12]
+console.log(`Your userarray is: ${userArray}`)
+const userNewArray = new Array(1,2,3,4)
+console.log(`Your usernewarray is: ${userNewArray}`)
+console.log(` Your userArray type is: ${typeof userArray}`)
